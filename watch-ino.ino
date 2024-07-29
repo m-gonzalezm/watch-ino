c0136261e8736a947339a0b172b722ca7932f375
Miguel <Miguel_a.GM@hotmail.com>
2024-07-29 06:46:47 -0600

    ci: design smart screen

--- watch-ino.ino
+++ watch-ino.ino
@@ -7,6 +7,10 @@
   -Specs-
   Testing board: Arduino Uno R3
   Display: OLED 0.96" 128x64
+  Software version: 0.1.0
+    + Design smart screen
+    Dev. beg 29.07.2024
+    Dev. end 29.07.2024
 
   -Connections-
   Pin   Component
@@ -24,6 +28,19 @@
 
 U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
+char hour[9], date[17];
+const char * weekdays[] = { "Sun.", "Mon.", "Tues.", "Wed.", "Thurs.", "Fri.", "Sat." };
+const char * months[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+void draw() {
+  sprintf(date, "%s, %s %d", weekdays[1], months[6], 29);
+  u8g2.setFont(u8g2_font_crox5h_tr);
+  u8g2.drawStr((128 - u8g2.getStrWidth(hour)) / 2, 30, hour);
+  u8g2.setFont(u8g2_font_crox1h_tr);
+  sprintf(hour, "%02d:%02d", 6, 0);
+  u8g2.drawStr((128 - u8g2.getStrWidth(date)) / 2, 50, date);
+}
+
 void setup() {
   u8g2.begin();
 }
@@ -31,7 +48,6 @@ void setup() {
 void loop() {
   u8g2.firstPage();
   do {
-    u8g2.setFont(u8g2_font_profont11_mr);
-    u8g2.drawStr(0, 10, "Hello world");
+    draw();
   } while(u8g2.nextPage());
 }
